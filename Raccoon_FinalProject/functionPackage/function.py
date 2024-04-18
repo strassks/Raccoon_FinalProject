fb99efe3b80130c4b443ad448cfe0fbcc5d2df57
conno <conno@DESKTOP-QMODCA8.wifi.uc.edu>
2024-04-18 13:37:09 -0400

    First Commit

--- Raccoon_FinalProject/functionPackage/function.py
+++ Raccoon_FinalProject/functionPackage/function.py
@@ -9,15 +9,5 @@
 # Citations:
 # Anything else that's relevant:
 
-#main.py
+#function.py
 
-file_path = "C:\\Users\\conno\\Downloads\\UCEnglish.txt"
-
-with open(file_path, "r") as file:
-    lines = file.readlines()
-
-line_numbers = [42061, 44404, 28799, 298, 8848, 27781, 105654, 21723, 47096, 8021, 28420, 19312, 22147, 42049, 23887, 599, 105655, 24232, 19312, 9443]
-
-for line_number in line_numbers:
-    line_content = lines[line_number ].strip()  # Adjusting for 0-based index
-    print(line_content)
