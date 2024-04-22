d27275c7cc74e8d75efed9950f0952a3450833cc
connorlaughlin <145033983+connorlaughlin@users.noreply.github.com>
2024-04-22 13:56:19 -0400

    Update main.py

--- Raccoon_FinalProject/mainPackage/main.py
+++ Raccoon_FinalProject/mainPackage/main.py
@@ -5,42 +5,28 @@
 # Course/Section: IS4010-001
 # Semester/Year: Spring 2024
 # Brief Description of the assignment: Decrypt text and json files to uncover details about taking a group photo on campus with a movie quote
-# Brief Description of what this module does:
-# Citations:
+# Brief Description of what this module does: Calls all the functions and classes that we created and prints desires results to console
+# Citations: Chat GPT
 # Anything else that's relevant:
 
 #main.py
 
+from TextFileExtractorPackage.TextFileExtractor import TextFileExtractor
+from ImageLoaderPackage.ImageLoaderClass import ImageLoader
 
+def main():
+    # TextFileExtractor
+    file_path = "../Files/UCEnglish.txt"
+    line_numbers = [42061, 44404, 28799, 298, 8848, 27781, 105654, 21723, 47096, 8021, 28420, 19312, 22147, 42049, 23887, 599, 105655, 24232, 19312, 9443]
+    TextFileExtractor(file_path, line_numbers)
+
+    # ImageLoader
+    image_path = "../Files/sample_image.jpg"
+    image_loader = ImageLoader(image_path)
+    image = image_loader.load_image()
+
+    # Print the image to the console
+    print(image)
 
 if __name__ == "__main__":
-    def main():
-        file_path = "../Files/UCEnglish.txt"
-        line_numbers = [42061, 44404, 28799, 298, 8848, 27781, 105654, 21723, 47096, 8021, 28420, 19312, 22147, 42049, 23887, 599, 105655, 24232, 19312, 9443]
-        TextFileExtractor(file_path, line_numbers)
-        
-        
-        
-        
-        
-        
-        
-        # 3  Aes Decryption
-        key = 'LMV69IGGTp2Gyn4TI-DTuupf0VvugeC5API5dpeoiqM='
-        decryptor = MessageDecryptor(key)
-        json_reader = JsonDataReader('../Files/TeamsAndEncryptedMessagesForDistribution.json')
-        movie_data = json_reader.get_raccoon_data()
-        strMovieMessage = ''.join([str(item) for item in movie_data])
-        # Replace 'your_encrypted_message_here' with the actual encrypted message
-        encrypted_message = strMovieMessage
-        # Decrypt the message
-        decrypted_message = decryptor.decrypt(encrypted_message)
-        # Display the result
-        print(decrypted_message)
-
-
-   
-    
-    
-    
-
+    main()
