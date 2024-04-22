69b00a120a6acde3bd93d883feb5b22aafa22982
cluth <cluth@DESKTOP-36GGSPM.wifi.uc.edu>
2024-04-22 13:47:46 -0400

    Added and completed a messageDecryptorPackage

--- Raccoon_FinalProject/mainPackage/main.py
+++ Raccoon_FinalProject/mainPackage/main.py
@@ -12,12 +12,36 @@
 #main.py
 
 from TextFileExtractorPackage.TextFileExtractor import TextFileExtractor
-
-def main():
-    file_path = "../Files/UCEnglish.txt"
-    line_numbers = [42061, 44404, 28799, 298, 8848, 27781, 105654, 21723, 47096, 8021, 28420, 19312, 22147, 42049, 23887, 599, 105655, 24232, 19312, 9443]
-    TextFileExtractor(file_path, line_numbers)
+from messageDecryptorPackage.messageDecryptor import*
 
 if __name__ == "__main__":
-    main()
+    def main():
+        file_path = "../Files/UCEnglish.txt"
+        line_numbers = [42061, 44404, 28799, 298, 8848, 27781, 105654, 21723, 47096, 8021, 28420, 19312, 22147, 42049, 23887, 599, 105655, 24232, 19312, 9443]
+        TextFileExtractor(file_path, line_numbers)
+        
+        
+        
+        
+        
+        
+        
+        # 3  Aes Decryption
+        key = 'LMV69IGGTp2Gyn4TI-DTuupf0VvugeC5API5dpeoiqM='
+        decryptor = MessageDecryptor(key)
+        json_reader = JsonDataReader('../Files/TeamsAndEncryptedMessagesForDistribution.json')
+        movie_data = json_reader.get_raccoon_data()
+        strMovieMessage = ''.join([str(item) for item in movie_data])
+        # Replace 'your_encrypted_message_here' with the actual encrypted message
+        encrypted_message = strMovieMessage
+        # Decrypt the message
+        decrypted_message = decryptor.decrypt(encrypted_message)
+        # Display the result
+        print(decrypted_message)
+
+
+   
+    
+    
+    
 
