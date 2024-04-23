ed3b96a7bf945b91b8e8f485db6dbe6d609e12be
conno <conno@DESKTOP-QMODCA8.wifi.uc.edu>
2024-04-22 22:36:18 -0400

    First Commit, with the correct Key because we were originally emailed the wrong key

---
+++ Raccoon_FinalProject/decryptionPackage/decryption.py
@@ -0,0 +1,38 @@
+# Name: Kaileb Strasser, Max Schiller, & Connor Laughlin
+# email: strassks@mail.uc.edu, schillms@mail.uc.edu, laughlcd@mail.uc.edu
+# Assignment Number: Final Project
+# Due Date: 4/23/2024
+# Course/Section: IS4010-001
+# Semester/Year: Spring 2024
+# Brief Description of the assignment: Decrypt text and json files to uncover details about taking a group photo on campus with a movie quote
+# Brief Description of what this module does: Calls all the functions and classes that we created and prints desires results to console
+# Citations: Chat GPT
+# Anything else that's relevant:
+
+#decryption.py
+
+import json
+from cryptography.fernet import Fernet
+from PIL import Image
+import os,sys
+from io import BytesIO
+
+def decrypt_location(encrypted_data, english_file):
+    with open(english_file, 'r', encoding='utf-8') as file:
+        english_words = file.readlines()
+    
+    decrypted_location = ""
+    for index in encrypted_data:
+        try:
+            line_number = int(index) # Adjusting to 0-based indexing
+            decrypted_location += english_words[line_number].strip() + " "
+        except IndexError:
+            print(f"Index {index} is out of range.")
+    
+    return decrypted_location.strip()
+
+def decrypt_message(encrypted_message, key):
+    f = Fernet(key)
+    decrypted_message = f.decrypt(encrypted_message)
+    return decrypted_message.decode('utf-8')
+
--- Raccoon_FinalProject/mainPackage/main.py
+++ Raccoon_FinalProject/mainPackage/main.py
@@ -1,5 +1,5 @@
 # Name: Kaileb Strasser, Max Schiller, & Connor Laughlin
-# email: strassks@mail.uc.edu, schillms@mail.uc.edu, laughlcd@mail.uc.edu
+# email: strassks@mail.uc.edu, schillmx@mail.uc.edu, laughlcd@mail.uc.edu
 # Assignment Number: Final Project
 # Due Date: 4/23/2024
 # Course/Section: IS4010-001
@@ -11,40 +11,28 @@
 
 #main.py
 
-from TextFileExtractorPackage.TextFileExtractor import TextFileExtractor
-from ImageLoaderPackage.ImageLoaderClass import ImageLoader
-from messageDecryptorPackage.messageDecryptor import MessageDecryptor
-from jsonReaderPackage.jsonReader import jsonReader
+from decryptionPackage.decryption import *
+from ImageLoaderPackage.ImageLoaderClass import *
+if __name__ == "__main__":
 
-# File paths
-text_file_path = "../Files/UCEnglish.txt"
-json_file_path = "../Files/TeamsAndEncryptedMessagesForDistribution - 001.json"
-image_file_path = "../Files/sample_image.jpg"
-
-# TextFileExtractor
-line_numbers = [42061, 44404, 28799, 298, 8848, 27781, 105654, 21723, 47096, 8021, 28420, 19312, 22147, 42049, 23887, 599, 105655, 24232, 19312, 9443]
-print("Extracted lines from text file:")
-TextFileExtractor(text_file_path, line_numbers)
-print()
-
-# MessageDecryptor
-key = b'LMV69IGGTp2Gyn4TI-DTuupf0VvugeC5API5dpeoiqM='
-decryptor = MessageDecryptor(key)
-json_reader = jsonReader(json_file_path)
-movie_data = json_reader.get_raccoon_data()
-str_movie_message = ''.join([str(item) for item in movie_data])
-encrypted_message = str_movie_message
-decrypted_message = decryptor.decrypt(encrypted_message)
-print("Decrypted message:")
-print(decrypted_message)
-print()
-
-# ImageLoader
-image_loader = ImageLoader(image_file_path)
-image = image_loader.load_image()
-
-# Print the image to the console
-print("Loaded image:")
-print(image)
-
-
+    encrypted_data = [42061, 44404, 28799, 298, 8848, 27781, 105654, 21723, 47096, 8021, 28420, 19312, 22147, 42049, 23887, 599, 105655, 24232, 19312, 9443]
+    
+    text_file = "../Files/UCEnglish.txt"
+    decrypted_location = decrypt_location(encrypted_data, text_file)
+    print("Decrypted location:", decrypted_location)
+    
+    # Decrypting the movie name
+    key = b'4g8xAE7lajIzp6UTyITiAGOYMSGzs5qKeMIdoT_js6U='
+    encrypted_movie = b'gAAAAABlTNM6fhccI70O7U91J2_HH6KjfNse0GcAqBZVV3h2ZO9TV8dXixIWR8UABjYYmchhPdoRnR-GlEcqJI7tY-ogTaf2fQ=='
+    decrypted_movie = decrypt_message(encrypted_movie, key)
+    print("Decrypted Movie Name:", decrypted_movie)
+    
+    # ImageLoader
+    image_loader = ImageLoader(image_file_path)
+    image = image_loader.load_image()
+    
+    # Print the image to the console
+    print("Loaded image:")
+    print(image)
+        
+    
