e69947f55ed9c8e0a0ac7be3ed7cbe87adf5001e
cluth <cluth@DESKTOP-36GGSPM.wifi.uc.edu>
2024-04-22 23:05:19 -0400

    Finished the ImageLoader Package

--- Raccoon_FinalProject/ImageLoaderPackage/ImageLoaderClass.py
+++ Raccoon_FinalProject/ImageLoaderPackage/ImageLoaderClass.py
@@ -12,4 +12,50 @@
 # ImageLoaderClass.py
 
 from PIL import Image
+class ImageLoader:
 
+   def __init__(self, file_path):
+
+       '''
+
+      Constructor for the ImageLoaderClass
+
+      @Param: file_path: The path to the image
+
+      '''
+
+       # Constructor to initialize the file path
+
+       self.file_path = file_path
+
+
+
+   def load_image(self):
+
+       '''
+
+       Returns an image
+
+       @Return: Image
+
+       '''
+
+       try:
+
+           # Try opening the image file using PIL's Image.open method
+
+           with Image.open(self.file_path) as img:
+
+               # Show the image using PIL's show method
+
+               img.show()
+
+               # Return the loaded image object
+
+               return img
+
+       except FileNotFoundError:
+
+           # Handle the case where the file is not found
+
+           print('Image not found')
